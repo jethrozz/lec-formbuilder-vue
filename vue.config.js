729b6c04308e53d3ece633aa79263892b869fd51
jethrozz <208499067@qq.com>
2020-11-10 00:07:50 +0800

    恢复preview配置

--- vue.config.js
+++ vue.config.js
@@ -27,13 +27,13 @@ module.exports = {
       chunks: ['chunk-vendors', 'chunk-common', 'index'],
       minify
     },
-    // preview: {
-    //   entry: 'src/views/preview/main.js',
-    //   template: 'public/preview.html',
-    //   filename: 'preview.html',
-    //   chunks: ['chunk-vendors', 'chunk-common', 'preview'],
-    //   minify
-    // }
+    preview: {
+      entry: 'src/views/preview/main.js',
+      template: 'public/preview.html',
+      filename: 'preview.html',
+      chunks: ['chunk-vendors', 'chunk-common', 'preview'],
+      minify
+    }
   },
   lintOnSave: false,
   css: {
